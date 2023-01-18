ac88bd9ae20bae6538dc7d5df61a645c66337bd7
Eno Agbasonu <105694642+udowanae@users.noreply.github.com>
2023-01-17 21:36:57 -0600

    Update variable.tf

--- 07 - Modules/example2-db-web-eip-sg/db/variable.tf
+++ 07 - Modules/example2-db-web-eip-sg/db/variable.tf
@@ -1,4 +1,4 @@
-
+# passing ssh key as a variable for the db server. Note that the ssh key but be created in the same AWS region ias the server
 variable "key_pair" {
   type        = string
   default     = "Class28key"
