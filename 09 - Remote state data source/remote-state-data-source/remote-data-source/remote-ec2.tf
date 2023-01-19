e3672c7cada01be6e2b718e39bfc32b19fb6acf7
Eno Agbasonu <105694642+udowanae@users.noreply.github.com>
2023-01-18 22:42:24 -0600

    Update remote-ec2.tf

--- 09 - Remote state data source/remote-state-data-source/remote-data-source/remote-ec2.tf
+++ 09 - Remote state data source/remote-state-data-source/remote-data-source/remote-ec2.tf
@@ -17,7 +17,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
     bucket = "my-terraformstate-landmark-buc"
-    key    = "terraform/terraform.tfstate"
+    key    = "terraform/terraform.tfstate"  # S3 path where the state file is stored
     region = "us-west-1"
   }
 }
@@ -25,14 +25,14 @@ data "terraform_remote_state" "network" {
 /*data "terraform_remote_state" "network" {
   backend = "local"
   config = {
-      path    = "../remote-data-source/terraform.tfstate"
+      path    = "../remote-data-source/terraform.tfstate"  # path where state file is stored locally
   }
 }*/
 
 resource "aws_instance" "my-ec2"{
   ami = data.aws_ami.amzlinux2.id
   instance_type = "t2.micro"
-  subnet_id = data.terraform_remote_state.network.outputs.public_subnets[1]
+  subnet_id = data.terraform_remote_state.network.outputs.public_subnets[1] # using the output data from the subnet created with the vpc. Since the subnet was outputted as a list, that's why we specified [1] at the end
 
   tags = {
     "Name" = "My_ec2"
